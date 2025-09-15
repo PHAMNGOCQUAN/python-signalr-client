559764f71efde33a74290153584a441ef7fcdd95
PHAMNGOCQUAN <ngoc.pham@studbocconi.it>
2025-09-15 12:09:06 +0700

    Update _transport.py

--- signalr_aio/transports/_transport.py
+++ signalr_aio/transports/_transport.py
@@ -84,7 +84,7 @@ class Transport:
     async def _master_handler(self, ws):
         consumer_task = asyncio.ensure_future(self._consumer_handler(ws))
         producer_task = asyncio.ensure_future(self._producer_handler(ws))
-        done, pending = await asyncio.wait([consumer_task, producer_task], return_when=asyncio.FIRST_EXCEPTION)
+        done, pending = await asyncio.wait([consumer_task, producer_task], return_when=asyncio.ALL_COMPLETED)
 
         for task in pending:
             task.cancel()
